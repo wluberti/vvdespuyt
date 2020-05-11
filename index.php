865291f38d5127ab18ee50e7329d4714849b9364
Wouter Luberti <wouter.luberti@xs4all.net>
2020-05-11 20:54:24 +0200

    basic template

--- index.php
+++ index.php
@@ -4,9 +4,9 @@ require_once 'vendor/autoload.php';
 
 $loader = new \Twig\Loader\FilesystemLoader('templates');
 $twig = new \Twig\Environment($loader, [
-    'cache' => 'var/cache',
+    // 'cache' => 'var/cache',
 ]);
 
-$template = $twig->load('index.twig');
+$template = $twig->load('home.twig');
 
 echo $template->render([]);
