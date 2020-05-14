5c83b42478da0651794b11902570b51abf2a4c29
Wouter Luberti <wouter.luberti@xs4all.net>
2020-05-14 22:24:57 +0200

    some cleanup for final version

--- deploy.php
+++ deploy.php
@@ -1,8 +1,17 @@
 <?php
+
+$filename = 'vendor.zip';
+
 $zip = new ZipArchive;
-if ($zip->open('vendor.zip') === TRUE) {
+if ($zip->open($filename) === TRUE) {
     echo $zip->extractTo('.');
     $zip->close();
+    try {
+        unlink($filename);
+        unlink(__FILE__);
+    } catch (Exception $exception) {
+        print($exception->getMessage());
+    }
 } else {
     echo 'failed';
 }
