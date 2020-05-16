e4a7fa32591ae519f32b8a7b1682e3c2388e0ff5
Wouter Luberti <wouter.luberti@xs4all.net>
2020-05-16 11:49:24 +0200

    got basic routing working

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
 
 require_once 'vendor/autoload.php';
+$templateDir = 'templates/';
 $notifications = [];
 
-$loader = new \Twig\Loader\FilesystemLoader('templates');
+$loader = new \Twig\Loader\FilesystemLoader($templateDir);
 $twig = new \Twig\Environment($loader, [
     // 'cache' => 'var/cache',
 ]);
@@ -17,14 +18,28 @@ if (isset($_GET["lang"])) {
         $lang = 'en';
     }
     setcookie($cookieName, $lang, time() + (86400 * 365), "/");
+    header('Location:/');
 } elseif (isset($_COOKIE[$cookieName])) {
     $lang = $_COOKIE[$cookieName];
 } else {
     $lang = 'nl'; // If no language prefence is found
 }
 
+// Detecting route to determine which template to load
+foreach (glob($templateDir . "[!_]*.twig") as $template) {
+    $foundTemplates[] = explode('/', $template)[1];
+}
+if (isset($_SERVER['REQUEST_URI'])) {
+    $requestedPage = explode('/', $_SERVER['REQUEST_URI'])[1] . ".twig";
+    if (in_array($requestedPage, $foundTemplates)) {
+        $displayPage = $requestedPage;
+    } else {
+        $displayPage = 'home.twig';
+    }
+}
+
 // Loading and displaying template
-$template = $twig->load('home.twig');
+$template = $twig->load($displayPage);
 echo $template->render([
     'lang' => $lang,
     'notifications' => $notifications,
